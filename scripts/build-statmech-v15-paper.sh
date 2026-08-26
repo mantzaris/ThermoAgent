9d4a01a6eac66abd4a71061749af398ef0b8b409
mantzaris <avmantzaris@gmail.com>
2026-08-26 14:54:54 -0400

    Reorganize JSTAT manuscript structure

--- scripts/build-statmech-v15-paper.sh
+++ scripts/build-statmech-v15-paper.sh
@@ -7,8 +7,4 @@ export FORCE_SOURCE_DATE="${FORCE_SOURCE_DATE:-1}"
 export TZ="${TZ:-UTC}"
 cd "$ROOT/paper/jstat_v15"
 latexmk -pdf -interaction=nonstopmode -halt-on-error main.tex
-if [[ -f supplement.tex ]]; then
-  latexmk -pdf -interaction=nonstopmode -halt-on-error supplement.tex
-  latexmk -c supplement.tex
-fi
 latexmk -c main.tex
